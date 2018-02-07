539b0b4563b53a9aee3cc33939524385cbd52c80
heavenel.cerna <heavenel.cerna@zone42-wb.cs.ucalgary.ca>
2018-02-06 18:40:23 -0700

    Updated

--- JavaComponent/task/Tasker.java
+++ JavaComponent/task/Tasker.java
@@ -6,92 +6,85 @@ import java.util.PriorityQueue;
 // Using Branch and Bound algorithm to solve Machine assignment problem
 public class Tasker {
 
-	
-	public  Tasker(int[][] costMatrix) {
-		
-		findMinCost(costMatrix);
-		
-	}
-	
 	// Calculates the least promising cost of node
 	// after Machine m is assigned to task t
-	private int calculateCost(int[][] costMatrix, int size, boolean[] assigned, int m, int t) {
+	private static int calculatePromisingCost(int[][] costMatrix, Node child) {
 		
-		int cost = 0;
-		boolean[] available = new boolean[size];
-		Arrays.fill(available, Boolean.TRUE);
+		int promisingCost = 0;
 		
-		// For each machine, starting from next machine
-		for (int i = m + 1; i < size; i++) {
-			int min = size + 1, minIndex = -1;
+		// Creates an array of available assignments set to TRUE
+		boolean[] availableNodesArray = new boolean[costMatrix.length];
+		Arrays.fill(availableNodesArray, Boolean.TRUE);
+		
+		// For each machine find the task that has the minimal penalty cost
+		for (int i = child.mach + 1; i < costMatrix.length; i++) {
+			int minTaskCost = Integer.MAX_VALUE, minTaskIndex = -1;
 			
-			// For each machine, starting from next machine
-			for (int j = 0; j < size; j++) {
+			// For each task
+			for (int j = 0; j < costMatrix.length; j++) {
 				// If task is unassigned
-				if (!assigned[j] && available[j] && costMatrix[i][j] < min) {
+				if (!child.assignedNodesArray[j] && availableNodesArray[j] && costMatrix[i][j] < minTaskCost) {
 					// Store task ID and cost
-					minIndex = j;
-					min = costMatrix[i][j];
+					minTaskIndex = j;
+					minTaskCost = costMatrix[i][j];
 				}
 			}
 			
 			// Add cost of next machine
-			cost += min;
+			promisingCost += minTaskCost;
 			
 			// Make optimal task unavailable
-			available[minIndex] = false;
+			if (minTaskIndex != -1)
+				availableNodesArray[minTaskIndex] = false;
 		}
 		
-		return cost;
+		return promisingCost;
 	}
 	
 	// Finds minimum cost using Branch and Bound algorithm
-	private void findMinCost(int[][] costMatrix) {
+	public static int optimize(int[][] costMatrix) {
 		
-		int size = costMatrix.length;
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+		PriorityQueue<Node> activeNodesArray = new PriorityQueue<Node>();
 		
-		// Initialize heap with a dummy node with cost 0
-		Node root = new Node(-1, -1, new boolean[size], null);
+		// Initialize heap with a dummy node with dummy values
+		Node root = new Node(costMatrix);
 		
 		// Adds a dummy node to list of live nodes;
-		pq.add(root);
+		activeNodesArray.add(root);
 		
-		while (!pq.isEmpty()) {
+		while (!activeNodesArray.isEmpty()) {
 			// Find a live node with least estimated cost 
 			// and delete from list of live nodes
-			Node minNode = pq.poll();
+			Node activeNode = activeNodesArray.poll();
 			
-			int m = minNode.machine + 1;
+			int currMach = activeNode.mach + 1;
 			
 			// If all machine are assigned to a task 
-			if (m == size) {
-				// Print Assignments
-				//return minNode.cost
-			}
+			if (currMach == costMatrix.length)
+				return activeNode.promisingCost;
 			
-			for (int t = 0; t < size; t++)
+			for (int currTask = 0; currTask < costMatrix.length; currTask++)
 		      {
 		        // If task is unassigned
-		        if (!minNode.assigned[t]) {
+		        if (!activeNode.assignedNodesArray[currTask]) {
 		          // Create a new tree node
-		          Node child = new Node(m, t, minNode.assigned, minNode);
+		          Node child = new Node(currMach, currTask, activeNode.assignedNodesArray, activeNode);
 		 
 		          // Cost for ancestor nodes plus current node
-		          child.pathCost = minNode.pathCost + costMatrix[m][t];
+		          child.pathCost = activeNode.pathCost + costMatrix[currMach][currTask];
 		 
 		          // Calculate node least promising cost
-		          child.cost = child.pathCost +
-		            calculateCost(costMatrix, size, child.assigned, m, t);
+		          child.promisingCost = child.pathCost +
+		            calculatePromisingCost(costMatrix, child);
 		 
 		          // Add child to list of live nodes;
-		          pq.add(child);
+		          activeNodesArray.add(child);
+		          
 		        }
+		  
 		      }
-			
-			
-			
 		}
+		return -1;
 	}
 	
 	// Node class representing space tree
@@ -102,34 +95,42 @@ public class Tasker {
 		// Contains cost for ancestors node
 		private int pathCost;
 		// Contains least promising cost
-		private int cost;
+		private int promisingCost;
 		// Contains worker ID
-		private int machine;
+		private int mach;
 		// Contains task ID
 		private int task;
 		// Contains information about available jobs
-		private boolean[] assigned;
+		private boolean[] assignedNodesArray;
+		
+		// Dummy node constructor
+		public Node(int[][] costMatrix) {
+			
+			this.assignedNodesArray = new boolean[costMatrix.length];
+			this.mach = -1;
+			this.task = -1;
+			this.parent = null;
+		}
 		
 		// Node creation constructor
 		public Node(int machine, int task, boolean[] assigned, Node parent) {
 			
-			this.assigned = new boolean[assigned.length];
+			this.assignedNodesArray = new boolean[assigned.length];
 			// Copies input assigned array to the node's assigned array
 			for (int i = 0; i < assigned.length; i++)
-				this.assigned[i] = assigned[i];
-			if (task != -1)
-				this.assigned[task] = true;
+				this.assignedNodesArray[i] = assigned[i];
 			
-			this.machine = machine;
+			this.assignedNodesArray[task] = true;
+			this.mach = machine;
 			this.task = task;
 			this.parent = parent;
 		}
 		
 		// Comparison method used by pQueue to compare and order nodes in queue
 		public int compareTo(Node n) {	
-			if (this.cost < n.cost)
+			if (this.promisingCost < n.promisingCost)
 	            return -1;
-	        else if(this.cost > n.cost)
+	        else if(this.promisingCost > n.promisingCost)
 	            return 1;
 	        else
 	            return 0;
